1c0c938200bf713dbad9de9257eedac4b6efa8e0
fer262008 <160235457+fer262008@users.noreply.github.com>
2025-04-29 12:16:44 +0000

    concluida aula 6.

--- scripit.js
+++ scripit.js
@@ -4,10 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const alternaContraste = document.getElementById('alterna-contraste')
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
     botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-       })
+    })
+
     alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste')
     })
